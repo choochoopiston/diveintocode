f17f5950e3509f610e9fa5a9fad49b14bfd40764
takayuki hara <choochoopiston@gmail.com>
2016-02-10 09:14:51 +0000

    inquiry design3

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,7 +2,7 @@ class Inquiry < ActiveRecord::Base
   
   validates :name,  
   presence: { message: "の入力は必須です。" },
-  length: { minimum: 15, message: "は3文字以上15文字以内で入力ください。" } 
+  length: { maximum: 15, message: "は3文字以上15文字以内で入力ください。" } 
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, 
