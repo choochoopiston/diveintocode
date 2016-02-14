4ff216009275eddad60b5a7bfe91f2627c77c1ec
takayuki hara <choochoopiston@gmail.com>
2016-02-14 12:16:12 +0000

    devise nihongoka 3

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Achieve
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    # Configure fallbacks for mongoid errors
+    config.i18n.fallbacks = {'ja' => 'en'}
   end
 end
