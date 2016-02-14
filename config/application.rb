c845c4ceaacbdeb6c665e7d343fdcfb42b48e80b
takayuki hara <choochoopiston@gmail.com>
2016-02-14 12:49:20 +0000

    login username delete

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Achieve
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
-    # Configure fallbacks for mongoid errors
-    config.i18n.fallbacks = {'ja' => 'en'}
+
   end
 end
