0a9938af643161a3f17885c30c57586678acdc22
takayuki hara <choochoopiston@gmail.com>
2016-02-29 07:35:09 +0000

    toukousha settei

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215131537) do
+ActiveRecord::Schema.define(version: 20160229063022) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "inquiries", force: :cascade do |t|
