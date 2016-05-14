f283ca44158846113efa38c45112627371bb97d9
castsmith <kcasting2016@gmail.com>
2016-05-14 09:36:28 +0000

    mailer

--- spec/requests/abouts_spec.rb
+++ spec/requests/abouts_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Abouts", type: :request do
+RSpec.describe "About Page", type: :request do
   describe "company_overview page" do
     it "should have the content '会社概要'" do
       visit '/about'
-      expect(response).to have_content("会社概要")
+      expect(page).to have_content("会社概要")
     end
   end
 end
