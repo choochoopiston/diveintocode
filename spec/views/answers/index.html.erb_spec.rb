5b0b3a7f5837ab2700ef74f6fcbb6b192ccbd8c0
choochoopiston <choochoopiston@gmail.com>
2016-06-06 01:01:49 +0000

    answer.route.setting

---
+++ spec/views/answers/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "answers/index", type: :view do
+  before(:each) do
+    assign(:answers, [
+      Answer.create!(
+        :content => "MyText",
+        :question => nil,
+        :user => nil
+      ),
+      Answer.create!(
+        :content => "MyText",
+        :question => nil,
+        :user => nil
+      )
+    ])
+  end
+
+  it "renders a list of answers" do
+    render
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+  end
+end
