a215db935e00cfe4aa872ba8647152997c95ff36
choochoopiston <choochoopiston@gmail.com>
2016-06-06 03:39:00 +0000

    answer.create.method redirect修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -25,10 +25,11 @@ class AnswersController < ApplicationController
   # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to question_path(@answer.question_id), notice: 'Answer was successfully created.' }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
