dc997a66c6eae54bdf05d79f12603298fd903243
choochoopiston <choochoopiston@gmail.com>
2016-06-08 13:17:30 +0000

    question&answer validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,8 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  validates :content,  
+  presence: { message: "入力してください。" }
+  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,10 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
   
+  validates :title,  
+  presence: { message: "入力してください。" }
+  
+  validates :content,  
+  presence: { message: "入力してください。" }
+  
 end
