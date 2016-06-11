a921bd6f5ebdabf8ef25cdb7546d68c768299a94
choochoopiston <choochoopiston@gmail.com>
2016-06-11 08:34:27 +0000

    relationship.model setting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606005458) do
+ActiveRecord::Schema.define(version: 20160611080615) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -62,6 +62,17 @@ ActiveRecord::Schema.define(version: 20160606005458) do
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
